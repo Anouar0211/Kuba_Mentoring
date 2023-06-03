e11c198b8d94ab691dd905c90e8bf3845060c7ec
Anouar <anouarsakji08@gmail.com>
2023-06-03 15:12:29 -0500

    addedNewStuff

---
+++ src/test/java/mentoring3/Practice2.java
@@ -0,0 +1,25 @@
+package mentoring3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Practice2 {
+
+    @Test
+    public void enter_info(){
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.tumblr.com/register/bird?redirectTo=%2F");
+
+
+}
